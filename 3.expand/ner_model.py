e962268777bb2ccdc00c3ccfd2acac593c609bb2
logan0116 <1809361150@qq.com>
2022-03-02 12:23:33 +0800

    logan-0302-update

---
+++ 3.expand/ner_model.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time    : 2022/2/28 上午10:41
+# @Author  : liu yuhan
+# @FileName: ner_model.py
+# @Software: PyCharm
+
+
+import torch
+import ipdb
+import numpy as np
+import torch.nn as nn
+import torch.optim as optim
+import torch.utils.data as Data
+
+from torch.autograd import Variable
+from torch.nn.utils import rnn
+from transformers import BertModel
+from torchcrf import CRF
+from sklearn.metrics import precision_score, recall_score, f1_score
+
+
+class MyDataSet(Data.Dataset):
+    '''
+    没啥说的，正常的数据载入
+    '''
+
+    def __init__(self, token_list, segment_list, mask_list, target_list, lens_list):
+        self.token_list = token_list
+        self.segment_list = segment_list
+        self.mask_list = mask_list
+        self.target_list = target_list
+        self.lens_list = lens_list
+
+    def __len__(self):
+        return len(self.token_list)
+
+    def __getitem__(self, idx):
+        return self.token_list[idx], self.segment_list[idx], self.mask_list[idx], self.target_list[idx], \
+               self.lens_list[idx]
+
+
+class MyNER(nn.Module):
+    def __init__(self, batch_size, num_tag, max_len, embed_dim, lstm_dim, lstm_layers, lstm_dropout, dropout):
+        super(MyNER, self).__init__()
+        self.batch_size = batch_size
+        self.max_len = max_len
+        self.lstm_dim = lstm_dim
+        self.lstm_layers = lstm_layers
+        # bert config
+        self.word_embeds = BertModel.from_pretrained('bert-base-uncased')
+        # Bilstm
+        self.lstm = torch.nn.LSTM(input_size=embed_dim, hidden_size=lstm_dim, num_layers=lstm_layers,
+                                  batch_first=True, dropout=lstm_dropout, bidirectional=True)
+        self.hidden = self.rand_init_hc()
+        self.cell = self.rand_init_hc()
+        # linear
+        self.dropout = nn.Dropout(p=dropout)
+        self.linear_lstm = torch.nn.Sequential(torch.nn.Linear(in_features=2 * lstm_dim, out_features=lstm_dim),
+                                               torch.nn.ReLU())
+        # linear-crf
+        self.linear_crf = torch.nn.Sequential(torch.nn.Linear(in_features=lstm_dim, out_features=num_tag),
+                                              torch.nn.ReLU())
+        self.crf = CRF(num_tag, batch_first=True)
+
+    def rand_init_hc(self):
+        """
+        random initialize hidden variable
+        """
+        return Variable(torch.randn(2 * self.lstm_layers, self.batch_size, self.lstm_dim)).cuda()
+
+    def forward(self, tokens_tensor, segments_tensor, attention_mask, lens):
+        # BERT
+        embeds = self.word_embeds(tokens_tensor, segments_tensor, attention_mask)[0]
+        # Bilstm
+        batch_packed = rnn.pack_padded_sequence(input=embeds, lengths=lens.cpu(),
+                                                batch_first=True, enforce_sorted=False)
+        lstm_out, _ = self.lstm(batch_packed, (self.hidden, self.cell))
+        output = rnn.pad_packed_sequence(lstm_out, batch_first=True, total_length=self.max_len)[0]
+        # linear
+        output = self.dropout(output)
+        output = self.linear_lstm(output)
+        # linear-crf
+        output = self.linear_crf(output)
+        return output
+
+    def loss_fun(self, feats, attention_mask, tag_tenser):
+        # loss
+        loss = -1 * self.crf(feats, tag_tenser, mask=attention_mask.byte())
+        return loss
+
+    def predict(self, feats, attention_mask):
+        # loss
+        pre_tenser = self.crf.decode(feats, mask=attention_mask.byte())
+        return pre_tenser
---
+++ 3.expand/ner_utils.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time    : 2022/3/1 上午10:59
+# @Author  : liu yuhan
+# @FileName: ner_utils.py
+# @Software: PyCharm
+import numpy as np
+import torch
+import json
+import pickle
+import os
+from transformers import BertTokenizer
+
+from tqdm import tqdm
+
+
+def not_empty(s):
+    return s and s.strip()
+
+
+def make_data(data_list, tag_dict, max_len):
+    data_size = len(data_list)
+
+    token_list = []
+    segment_list = []
+    mask_list = []
+    lens_list = []
+    target_list = []
+
+    # 通过词典导入分词器
+    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+
+    # token_ids & target_ids
+    for value in tqdm(data_list):
+        # cut
+        token, target = value
+        token_trans, target_trans = [], []
+        for word, tag in zip(token, target):
+            token_trans.append(tokenizer.convert_tokens_to_ids(word.lower()))
+            target_trans.append(tag_dict[tag])
+
+        mask = [1] * len(token_trans)
+        segment = [0] * len(target_trans)
+
+        token_list.append(token_trans)
+        segment_list.append(segment)
+        mask_list.append(mask)
+        target_list.append(target_trans)
+        lens_list.append(len(token_trans))
+
+    # 补pad
+    for i in range(data_size):
+        token_list[i].extend([0] * (max_len - lens_list[i]))
+        segment_list[i].extend([0] * (max_len - lens_list[i]))
+        mask_list[i].extend([0] * (max_len - lens_list[i]))
+        target_list[i].extend([0] * (max_len - lens_list[i]))
+
+    return torch.LongTensor(token_list), torch.LongTensor(segment_list), torch.LongTensor(mask_list), \
+           torch.LongTensor(target_list), torch.LongTensor(lens_list)
+
+
+def get_accuracy(pre_tenser, tag_tenser):
+    acc_list = []
+    for p_t, t_t in zip(pre_tenser, tag_tenser):
+        num_tag = 0
+        num_tag_acc = 0
+        for bit in range(len(p_t)):
+            if t_t[bit] == 17:
+                continue
+            num_tag += 1
+            if t_t[bit] == p_t[bit]:
+                num_tag_acc += 1
+        if num_tag == 0:
+            continue
+        acc_list.append(num_tag_acc / num_tag)
+    return np.mean(acc_list)
+
+
+def make_path(path):
+    if not os.path.exists(path):
+        os.mkdir(path)
